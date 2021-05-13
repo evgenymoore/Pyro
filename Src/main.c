2e96cd9e1220f5d1f0681690a5ff89eaa5211902
Evgeny Murashov <github.com/evgenymoore>
2021-05-13 13:15:24 +0300

    Доработка протокола

--- Src/main.c
+++ Src/main.c
@@ -21,10 +21,10 @@ int main(void)
   Axel.Write(INT_ENABLE, 0x10);
   Axel.Write(POWER_CTL, 0x08);
   
-  TIM7_Init((uint16_t)(SystemCoreClock / 8000), 20);
-  TIM_Enable(TIM7);
-
   TIM6_Init((uint16_t)(SystemCoreClock / 1000), 12);
+  TIM7_Init((uint16_t)(SystemCoreClock / 1000), 16);
+  TIM_Enable(TIM7);
+  
   UART.Init();  
   UART.Receive();
   
